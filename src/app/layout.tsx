5c9d16948a12c000976a98784a109695eda3cca1
robertotics4 <robertooliveiragp@gmail.com>
2024-04-15 16:03:00 -0300

    create .env.example

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,25 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { AuthProvider } from "./providers/auth-provider";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { AuthProvider } from './providers/auth-provider'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Playfieldz",
-  description: "Organize sua pelada!",
-};
+  title: 'Playfieldz',
+  description: 'Organize sua pelada!',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <AuthProvider>
       <html lang="en">
-        <body className={inter.className}>
-          {children}
-        </body>
+        <body className={inter.className}>{children}</body>
       </html>
     </AuthProvider>
-
-  );
+  )
 }
