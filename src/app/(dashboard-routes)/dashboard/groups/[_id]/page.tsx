89b83555ac353b6b02c72f255796038ef2cbd2ed
robertotics4 <robertooliveiragp@gmail.com>
2024-04-18 17:29:03 -0300

    feat: dynamic group player

--- src/app/(dashboard-routes)/dashboard/groups/[_id]/page.tsx
+++ src/app/(dashboard-routes)/dashboard/groups/[_id]/page.tsx
@@ -1,11 +1,60 @@
 'use client'
 
-import { GroupCardInfo } from '@/app/components/GroupCard/GroupCardInfo'
 import { useParams } from 'next/navigation'
+import { useSession } from 'next-auth/react'
 import { GroupPlayer } from '@/app/components/GroupCard/GroupPlayer'
+import { GroupService } from '@/app/services/groups/GroupService'
+import { GroupCardInfo } from '@/app/components/GroupCard/GroupCardInfo'
+import { PlayerSubscription } from '@/app/types/entities/Group'
+
+const subscriptions = [
+  {
+    player: {
+      _id: '66146b8467bc0755c63e811c',
+      nickname: 'Caneta',
+      age: 32,
+      position: 'DM',
+      userId: '66146b8367bc0755c63e811a',
+      attributes: [
+        {
+          name: 'Defesa',
+          value: 5,
+          _id: '66146b8467bc0755c63e811d',
+        },
+        {
+          name: 'Assistência',
+          value: 5,
+          _id: '66146b8467bc0755c63e811e',
+        },
+        {
+          name: 'Drible',
+          value: 5,
+          _id: '66146b8467bc0755c63e811f',
+        },
+        {
+          name: 'Chute',
+          value: 5,
+          _id: '66146b8467bc0755c63e8120',
+        },
+        {
+          name: 'Habilidades',
+          value: 5,
+          _id: '66146b8467bc0755c63e8121',
+        },
+      ],
+      createdAt: '2024-04-08T22:11:16.269Z',
+      updatedAt: '2024-04-08T22:11:16.269Z',
+      __v: 0,
+    },
+    paymentRecurrence: 'Mensalista',
+    _id: '66216c27cbda5ebf59926b28',
+  },
+]
 
 export default function GroupPage() {
   const params = useParams<{ _id: string }>()
+  const session = useSession()
+  const groupService = GroupService.getInstance()
 
   return (
     <div>
@@ -14,15 +63,12 @@ export default function GroupPage() {
       <div className="flex flex-col gap-6">
         <h1 className="font-bold mt-12 text-xl text-slate-800">Jogadores</h1>
 
-        <GroupPlayer />
-        <GroupPlayer />
-        <GroupPlayer />
-        <GroupPlayer />
-        <GroupPlayer />
-        <GroupPlayer />
-        <GroupPlayer />
-        <GroupPlayer />
-        <GroupPlayer />
+        {subscriptions.map((sub) => (
+          <GroupPlayer
+            key={sub._id}
+            subscriptionData={sub as PlayerSubscription}
+          />
+        ))}
       </div>
     </div>
   )
--- src/app/components/GroupCard/GroupPlayer/index.tsx
+++ src/app/components/GroupCard/GroupPlayer/index.tsx
@@ -1,6 +1,17 @@
 import { EMPTY_PICTURE_DEFAULT } from '@/app/support/constants'
+import {
+  PlayerPaymentRecurrence,
+  PlayerSubscription,
+} from '@/app/types/entities/Group'
 
-export function GroupPlayer() {
+type GroupPlayerProps = {
+  subscriptionData: PlayerSubscription
+}
+
+export function GroupPlayer({ subscriptionData }: GroupPlayerProps) {
+  const { paymentRecurrence } = subscriptionData
+  const { nickname, age, position, attributes } = subscriptionData.player
+
   return (
     <div className="flex gap-4 items-center">
       <img
@@ -11,17 +22,23 @@ export function GroupPlayer() {
 
       <div className="flex flex-col gap-2">
         <p className="font-bold text-normal text-slate-800">
-          Caneta, 32 anos, Volante (DM)
+          {nickname}, {age} anos, {position.toString()} ({position.toString()})
         </p>
 
         <p className="text-zinc-500">
-          Def: 5 / Ass: 5 / Drib: 5 / Chute: 5 / Hab: 5
+          {attributes.map((att, index) => {
+            if (index === 0) return `${att.name}: ${att.value}`
+
+            return ` - ${att.name}: ${att.value}`
+          })}
         </p>
       </div>
 
       <div className="ml-auto flex flex-col items-center">
         <span className="font-bold text-xl text-emerald-800">5.0</span>
-        <span className="font-bold text-xl text-emerald-800">M</span>
+        <span className="font-bold text-xl text-emerald-800">
+          {paymentRecurrence === PlayerPaymentRecurrence.MONTHLY ? 'M' : 'D'}
+        </span>
       </div>
     </div>
   )
